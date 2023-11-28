de00b3f710afa431c797dc8dee1248f89bd5fc6f
Nataliia-Nikolaienko <natalina_pretty@ukr.net>
2023-11-28 12:16:40 +0100

    summary

--- src/App.jsx
+++ src/App.jsx
@@ -1,98 +1,77 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import ContactForm from './components/ContactForm';
 import ContactList from './components/ContactList';
 import Filter from 'components/Filter';
 import { nanoid } from 'nanoid';
 import css from './components/ContactForm.module.css';
 
-class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+const App = () => {
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(window.localStorage.getItem('my-contacts')) ?? []
+  );
+  const [filter, setFilter] = useState('');
 
-  addContact = ({ newContact, name, number }) => {
-    const contactObj = {
-      ...newContact,
-      id: nanoid(),
-      name,
-      number,
-    };
-    const nameContact = this.state.contacts.find(
-      contact => contact.name === name
-    );
+  const addContact = ({ name, number }) => {
+    const nameContact = contacts.find(contact => contact.name === name);
+    const numberContact = contacts.find(contact => contact.number === number);
     if (nameContact) {
       return alert(`${name} is already in contacts.`);
+    } else if (numberContact) {
+      return alert(`${number} is already in contacts.`);
+    } else {
+      setContacts(prevContacts => {
+        const newContact = {
+          id: nanoid(),
+          name,
+          number,
+        };
+        return [...prevContacts, newContact];
+      });
     }
-
-    this.setState(prev => ({
-      contacts: [...prev.contacts, contactObj],
-    }));
   };
 
-  deleteContact = id => {
-    this.setState(prev => ({
-      contacts: prev.contacts.filter(contact => contact.id !== id),
-    }));
+  const deleteContact = id => {
+    setContacts(prevContacts => {
+      return prevContacts.filter(contact => contact.id !== id);
+    });
   };
 
-  changeFilter = e => {
-    this.setState({ filter: e.currentTarget.value });
+  const changeFilter = e => {
+    setFilter(e.currentTarget.value);
   };
 
-  componentDidMount() {
-    const contacts = localStorage.getItem('my contacts');
-    const parsedContacts = JSON.parse(contacts);
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
-    }
-  }
+  useEffect(() => {
+    window.localStorage.setItem('my-contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('my contacts', JSON.stringify(this.state.contacts));
-    }
-  }
+  const normalizedFilter = filter.toLowerCase();
+  const visibleContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(normalizedFilter)
+  );
 
-  render() {
-    const { contacts, filter } = this.state;
-    const normalizedFilter = filter.toLowerCase();
-    const visibleContacts = contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter)
-    );
-
-    return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          justifyContent: 'space-evenly',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        <div className={css.formWrapper}>
-          <h1 className={css.phonebookTitle}>Phonebook</h1>
-          <ContactForm addContact={this.addContact} />
-        </div>
-
-        <div className={css.contactsWrapper}>
-          <h2 className={css.contactsTitle}>Contacts</h2>
-          <Filter value={filter} onChange={this.changeFilter} />
-          <ContactList
-            contacts={visibleContacts}
-            deleteContact={this.deleteContact}
-          />
-        </div>
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      <div className={css.formWrapper}>
+        <h1 className={css.phonebookTitle}>Phonebook</h1>
+        <ContactForm addContact={addContact} />
       </div>
-    );
-  }
-}
+
+      <div className={css.contactsWrapper}>
+        <h2 className={css.contactsTitle}>Contacts</h2>
+        <Filter value={filter} onChange={changeFilter} />
+        <ContactList contacts={visibleContacts} deleteContact={deleteContact} />
+      </div>
+    </div>
+  );
+};
 
 export default App;
-
-// const isExist = contacts.map(contact => contact.name);
-//     if (isExist === name) {
-//       return alert(`${name} is already in contacts.`);
-//     }
